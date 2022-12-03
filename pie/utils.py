f4fb11df8c86d216a76ea1ba7b72400d8a20bf8e
Blake Searl <blake.searl@sonocent.com>
2022-12-03 16:59:58 +0000

    util simplifications

--- pie/utils.py
+++ pie/utils.py
@@ -2,8 +2,12 @@ import os
 from typing import List, Tuple
 
 
+def file_path(filename: str) -> str:
+    return f"{os.getcwd()}/pie/{filename}"
+
+
 def file_to_string_list(filename: str) -> List[str]:
-    with open(f"{os.getcwd()}/pie/{filename}") as f:
+    with open(file_path(filename)) as f:
         return [line.rstrip() for line in f]
 
 
@@ -12,22 +16,8 @@ def empty_line_separated_group_to_int_lists(filename: str) -> List[List[int]]:
     return [[int(item) for item in string_list] for string_list in string_lists]
 
 
-def empty_line_separated_group_to_string_lists(
-    filename: str,
-) -> List[List[str]]:
-    string_list = file_to_string_list(filename)
-
-    groups = []
-    current_group = []
-    for item in string_list:
-        if item == "":
-            groups.append(current_group)
-            current_group = []
-        else:
-            current_group.append(item)
-
-    groups.append(current_group)
-    return groups
+def empty_line_separated_group_to_string_lists(filename: str) -> List[List[str]]:
+    return [x.split("\n") for x in open(file_path(filename)).read().strip().split("\n\n")]
 
 
 def file_to_string_pairs(filename: str) -> List[Tuple[str, str]]:
