22153c2abee4aa566503fa95909fb10aee751d2e
Blake Searl <blake.searl@sonocent.com>
2022-12-14 17:42:37 +0000

    mess around with classes

--- pie/eleven/__main__.py
+++ pie/eleven/__main__.py
@@ -1,53 +1,60 @@
 import math
 import operator
 import re
-from typing import Callable
+from typing import Callable, List, Tuple
 
 from pie.file_utils import read_input
 
 
-def get_handler(op_str: str) -> Callable[[int], int]:
-    _, mid, right = op_str.split()
+class Monkey:
+    def __init__(self, inventory: List[int], handler: Callable[[int], int], modulo: int, if_true: int, if_false: int):
+        self.inventory = inventory
+        self.handler = handler
+        self.modulo = modulo
+        self.if_true = if_true
+        self.if_false = if_false
+        self.inspections = 0
 
-    def handler(x: int) -> int:
-        operation = operator.add if mid == "+" else operator.mul
-        rhs = int(right) if right.isdigit() else x
+    @classmethod
+    def from_story(cls, story: str):
+        _, inv, op, *test = story.split("\n")
+        inventory = [int(x) for x in re.findall("[0-9]+", inv)]
+        handler = cls.get_handler(op)
+        modulo, if_true, if_false = [int(x) for rule in test for x in re.findall("[0-9]+", rule)]
 
-        return operation(x, rhs)
+        return cls(inventory, handler, modulo, if_true, if_false)
 
-    return handler
+    @classmethod
+    def get_handler(cls, op):
+        _, mid, right = op.split(" = ")[-1].split()
 
+        def handler(x: int) -> int:
+            operation = operator.add if mid == "+" else operator.mul
+            rhs = int(right) if right.isdigit() else x
+            return operation(x, rhs)
 
-def get_monkeys():
-    question = read_input("eleven/input.txt").split("\n\n")
-    monkeys = []
-    for group in question:
-        _, inventory, op, *test = group.split("\n")
-        inventory = [int(x) for x in re.findall("[0-9]+", inventory)]
-        handler = get_handler(op.split(" = ")[-1])
+        return handler
 
-        check, if_true, if_false = [int(x) for rule in test for x in re.findall("[0-9]+", rule)]
-        monkeys.append((inventory, handler, check, if_true, if_false))
-
-    return monkeys
+    def inspect(self, lcm: int, reduce_stress=True) -> Tuple[int, int]:
+        item = self.handler(self.inventory.pop(0)) % lcm
+        if reduce_stress:
+            item //= 3
+        self.inspections += 1
+        target_monkey = self.if_true if item % self.modulo == 0 else self.if_false
+        return item, target_monkey
 
 
 def exec_rounds(part_two: bool):
-    monkeys = get_monkeys()
-    inspections = {idx: 0 for idx, _ in enumerate(monkeys)}
-    lcm = math.lcm(*[monkey[2] for monkey in monkeys])
+    monkeys = [Monkey.from_story(story) for story in read_input("eleven/input.txt").split("\n\n")]
+    lcm = math.lcm(*[monkey.modulo for monkey in monkeys])
     for _ in range(10000 if part_two else 20):
-        for idx, monkey in enumerate(monkeys):
-            inventory, handler, check, if_true, if_false = monkey
-            while len(inventory) > 0:
-                item = handler(inventory.pop(0)) % lcm
-                if not part_two:
-                    item //= 3
-                inspections[idx] += 1
-                monkeys[if_true if item % check == 0 else if_false][0].append(item)
+        for monkey in monkeys:
+            while len(monkey.inventory) > 0:
+                item, target_monkey = monkey.inspect(lcm, not part_two)
+                monkeys[target_monkey].inventory.append(item)
 
-    inspection_counts = sorted(inspections.values(), reverse=True)
-    print(inspection_counts[0] * inspection_counts[1])
+    inspection_counts = sorted([monkey.inspections for monkey in monkeys])
+    print(inspection_counts[-1] * inspection_counts[-2])
 
 
 if __name__ == "__main__":
