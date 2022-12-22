4e9dd0b76675439d37f8bd9f04913b920f938e06
Blake Searl <blake.searl@sonocent.com>
2022-12-22 16:46:58 +0000

    refactor to include part one

--- pie/twentyone/__main__.py
+++ pie/twentyone/__main__.py
@@ -6,17 +6,26 @@ from pie.file_utils import read_lines
 
 
 def find_error_for_equations(replaced_equations: List[str]) -> int:
+    root = calc_root_from_equations(replaced_equations)
+    return sign(root)
+
+
+def calc_root_from_equations(replaced_equations):
     loc = {}
-    finished = False
-    while not finished:
-        finished = True
+    substitutions_remaining = True
+    while substitutions_remaining:
+        substitutions_remaining = False
         for equation in replaced_equations:
             try:
                 exec(equation, None, loc)
             except NameError:
-                finished = False
+                substitutions_remaining = True
+    return loc["root"]
 
-    return sign(loc["root"])
+
+def part_one():
+    equations = [line.replace(": ", " = ") for line in read_lines("twentyone/input.txt")]
+    print(calc_root_from_equations(equations))
 
 
 def part_two():
@@ -24,7 +33,7 @@ def part_two():
     without_humn = [
         line.replace("+", "-") if "root" in line else line for line in equations if not line.startswith("humn")
     ]
-    humn_range = (0, 9_999_999_999_999)
+    estimate, humn_range = 0, (0, 9_999_999_999_999)
     located = False
     while not located:
         humn_range_low, humn_range_high = humn_range
@@ -32,13 +41,15 @@ def part_two():
         estimate_equations = [*without_humn, f"humn = {estimate}"]
         error = find_error_for_equations(estimate_equations)
         if error == 0:
-            print(estimate)
-            break
+            located = True
         elif error < 0:
             humn_range = (humn_range_low, estimate)
         elif error > 0:
             humn_range = (estimate, humn_range_high)
 
+    print(estimate)
+
 
 if __name__ == "__main__":
+    part_one()
     part_two()
