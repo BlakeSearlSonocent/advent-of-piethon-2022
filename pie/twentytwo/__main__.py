9433b51534671b62ad0f0a2073e52beb518e1537
Blake Searl <blake.searl@sonocent.com>
2022-12-22 12:24:36 +0000

    twentytwo using complex numbers

--- pie/twentytwo/__main__.py
+++ pie/twentytwo/__main__.py
@@ -2,47 +2,32 @@ import os
 import re
 
 if __name__ == "__main__":
-    puzzle, cmds = [x.split("\n") for x in open(f"{os.getcwd()}/pie/twentytwo/input.txt").read().split("\n\n")]
-    cols = len(puzzle[0])
-    puzzle = [line.ljust(cols) for line in puzzle]
-    rows = len(puzzle)
+    puzzle_input, commands_input = [
+        x.split("\n") for x in open(f"{os.getcwd()}/pie/twentytwo/input.txt").read().split("\n\n")
+    ]
+    puzzle = {complex(x, y): val for y, row in enumerate(puzzle_input) for x, val in enumerate(row) if val in ".#"}
+    commands = re.findall("[0-9]+|[RL]", commands_input[0])
 
-    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    commands = re.findall("[0-9]+|[RL]", cmds[0])
+    position = puzzle_input[0].index(".")
+    direction = 1
 
-    position = (min(i for i, val in enumerate(puzzle[0]) if val == "."), 0)
-    direction = 0
-
-    for cmd in commands:
-        if cmd == "R":
-            direction += 1
-        elif cmd == "L":
-            direction -= 1
+    for command in commands:
+        if command == "R":
+            direction *= 1j
+        elif command == "L":
+            direction *= -1j
         else:
-            count = int(cmd)
-            direction_x, direction_y = directions[direction % len(directions)]
-
+            count = int(command)
             for _ in range(count):
-                position_x, position_y = position
-                potential_x, potential_y = (position_x + direction_x) % cols, (position_y + direction_y) % rows
+                potential_position = position + direction
+                if potential_position not in puzzle:
+                    # reverse across the row or column
+                    while potential_position - direction in puzzle:
+                        potential_position = potential_position - direction
 
-                if puzzle[potential_y][potential_x] == " ":
-                    if (direction_x, direction_y) == (1, 0):
-                        potential_x = min(i for i, val in enumerate(puzzle[potential_y]) if val != " ")
-                    if (direction_x, direction_y) == (-1, 0):
-                        potential_x = max(i for i, val in enumerate(puzzle[potential_y]) if val != " ")
-                    if (direction_x, direction_y) == (0, 1):
-                        column = [val for y in range(rows) for val in puzzle[y][potential_x]]
-                        potential_y = min(j for j, val in enumerate(column) if val != " ")
-                    if (direction_x, direction_y) == (0, -1):
-                        column = [val for y in range(rows) for val in puzzle[y][potential_x]]
-                        potential_y = max(j for j, val in enumerate(column) if val != " ")
-
-                if puzzle[potential_y][potential_x] == "#":
+                if puzzle[potential_position] == "#":
                     break
 
-                position = (potential_x, potential_y)
+                position = potential_position
 
-    final_direction = directions[direction % len(directions)]
-    direction_score = direction % len(directions)
-    print(((position[1] + 1) * 1000) + ((position[0] + 1) * 4) + direction_score)
+    print(int(((position.imag + 1) * 1000) + ((position.real + 1) * 4) + [1, 1j, -1, -1j].index(direction)))
