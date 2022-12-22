278d3e641b7098bb162c78ec3d6dfe7122c8ae5c
Blake Searl <blake.searl@sonocent.com>
2022-12-22 16:39:34 +0000

    more tidy

--- pie/twentyone/__main__.py
+++ pie/twentyone/__main__.py
@@ -5,30 +5,23 @@ from numpy import sign
 from pie.file_utils import read_lines
 
 
-def find_error_for_estimate(estimate: int, replaced_equations: List[str]) -> int:
-    replaced_equations.append(f"humn = {estimate}")
-
-    finished = False
+def find_error_for_equations(replaced_equations: List[str]) -> int:
     loc = {}
+    finished = False
     while not finished:
         finished = True
         for equation in replaced_equations:
             try:
                 exec(equation, None, loc)
-
             except NameError:
                 finished = False
 
-    root = loc["root"]
-    if root == 0:
-        return 0
-    else:
-        return sign(root)
+    return sign(loc["root"])
 
 
 def part_two():
     equations = [line.replace(": ", " = ") for line in read_lines("twentyone/input.txt")]
-    replaced_equations = [
+    without_humn = [
         line.replace("+", "-") if "root" in line else line for line in equations if not line.startswith("humn")
     ]
     humn_range = (0, 9_999_999_999_999)
@@ -36,7 +29,8 @@ def part_two():
     while not located:
         humn_range_low, humn_range_high = humn_range
         estimate = (humn_range_low + humn_range_high) // 2
-        error = find_error_for_estimate(estimate, replaced_equations)
+        estimate_equations = [*without_humn, f"humn = {estimate}"]
+        error = find_error_for_equations(estimate_equations)
         if error == 0:
             print(estimate)
             break
